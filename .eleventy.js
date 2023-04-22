9eb00e8ffcbe01fb35dbd377ff88b2a28e80bce7
Logan Woolf <loganwoolf@gmail.com>
2023-04-21 23:28:54 -0600

    Add tailwind to project

---
+++ .eleventy.js
@@ -0,0 +1,10 @@
+const now = String(Date.now());
+
+module.exports = function (eleventyConfig) {
+  eleventyConfig.addWatchTarget("./styles/tailwind.config.js");
+  eleventyConfig.addWatchTarget("./styles/tailwind.css");
+
+  eleventyConfig.addPassthroughCopy({ "./_tmp/style.css": "./style.css" });
+
+  eleventyConfig.addShortcode("version", () => now);
+};
